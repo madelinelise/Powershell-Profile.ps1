cdb6f768f569caf4e93bc0dd58e76cc35e550597
Madeline Schimenti <madelinelise@gmail.com>
2015-08-25 19:27:35 -0400

    Fixed Powershell Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Rename-Item Function:\Prompt PoshGitPrompt -Force
+function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
+if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
